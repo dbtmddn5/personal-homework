a50d18314a036b6c4570efe957d2bfdde6a85c4b
SEUNGWOO YU <k19rs140@st.kyusan-u.ac.jp>
2024-02-07 17:24:02 +0900

    latest version

--- level2/multiply_table3.php
+++ level2/multiply_table3.php
@@ -4,6 +4,9 @@ function isPrime($num) {
     if($num == 1) {
         return true;
     }
+    if($num == 2) {
+        return true;
+    }
     for($i = 2; $i <= sqrt($num); $i++) {
         if($num % $i == 0) {
             return false;
